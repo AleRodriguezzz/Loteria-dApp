5d67d16c797fdc35b14b9fe5b23809259c86240b
Ale Rodriguez <alejandro.ma43996@cesurformacion.com>
2022-06-13 20:37:55 +0200

    Funcion boton MetaMask añadida, falta boton desaparecer

--- Loteria/loteria-dapp/pages/components/header.js
+++ Loteria/loteria-dapp/pages/components/header.js
@@ -1,5 +1,26 @@
+import Web3 from 'web3'
+import { useState } from 'react'
 import styles from '../../styles/Home.module.css'
 function HeaderComponent(){
+    const [web3, setWeb3] = useState()
+    const [cuentas, setCuentas] = useState()
+    const connectWalletHandler = async () => {
+        if(typeof window !== "undefined" && typeof window.ethereum !== "undefined"){
+            try{
+                await window.ethereum.request({method: "eth_requestAccounts"})
+                const web3 = new Web3(window.ethereum)
+                setWeb3(web3)
+                const cuentas = await web3.eth.getAccounts()
+                setCuentas(cuentas[0])
+                const btn = document.getElementById('metamaskbtn')
+                btn.style.display = "none"
+            }catch(err){
+                console.log(err.message)
+            }
+        }else{
+            console.log("Instale MetaMask")
+        }
+    }
     return(
         <header className={styles.header}>
             <div className={styles.title}>
@@ -13,7 +34,7 @@ function HeaderComponent(){
                 </ul>
             </nav>
             <div>
-                <button>Conectar Wallet</button>
+                <button id='metamaskbtn' onClick={connectWalletHandler}>Conectar Wallet</button>
             </div>
       </header>
     )
